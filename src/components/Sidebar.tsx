1df1d7736da96d062bc7ace5e32f21e486ac135d
Martin Ezequiel Williner <williner.dev@gmail.com>
2025-01-04 21:40:23 -0300

    feat: deleted unused div

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -47,13 +47,6 @@ export const Sidebar = () => {
           
         </ul>
       </div>
-
-      <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
-        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-          <CiLogout />
-          <span className="group-hover:text-gray-700">Logout</span>
-        </button>
-      </div>
     </aside>
   )
 }
